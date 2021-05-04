ddc48d8f83fcf32e987e151d3b9a7c4a3263e21a
Simplex-hub <majeedolaitan87@gmail.com>
2021-05-04 16:36:37 +0100

    Resposive Web Page Task

---
+++ script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var m = document.getElementById("myNavbar");
+    if (m.className === "nav-bar") {
+        x.className += "responsive";
+    } else {
+        x.className = "nav-bar";
+    }
+}
